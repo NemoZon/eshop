6a537e85875f3998167d1955be1052fd3ad59f9f
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2025-01-17 13:59:55 +0100

    feat: login & reg forms

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\RegisterUserType;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('register/index.html.twig');
+        $user = new User();
+        $form = $this->createForm(RegisterUserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('register/index.html.twig', [
+            'registerForm' => $form->createView(),
+        ]);
     }
 }
--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -3,26 +3,69 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse mail',
+                'attr' => ['placeholder' => 'Entrez votre adresse mail'],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                ],
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => ['placeholder' => 'Entrez votre mot de passe'],
+                    'hash_property_path' => 'password',
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => ['placeholder' => 'Confirmez votre mot de passe'],
+                ],
+                'mapped' => false,
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'First Name',
+                'attr' => ['placeholder' => 'Enter your first name']
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Last Name',
+                'attr' => ['placeholder' => 'Enter your last name']
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-success'],
+            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'constraints' => [
+                new UniqueEntity([
+                    'entityClass' => User::class,
+                    'fields' => 'email',
+                ])
+            ],
             'data_class' => User::class,
         ]);
     }
